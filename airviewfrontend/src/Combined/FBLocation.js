bb01bc350380566065be07410868cf56f49d4f0b
Ms.THANAWAN PANAPONGPAISAN <suithanawan@gmail.com>
2023-09-18 22:48:20 +0700

    Update FBLocation.js

--- airviewfrontend/src/Combined/FBLocation.js
+++ airviewfrontend/src/Combined/FBLocation.js
@@ -3,7 +3,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Grid'; // Import Grid
 import { Typography } from '@mui/material';
 import LGStation from '../emrontech/learningGarden/station';
 import FiboStation from '../emrontech/fibo/station';
@@ -14,10 +14,9 @@ import LGFullscreenContent from '../dashboard/content/LGfull';
 function FBLocation() {
     const [selectedStation, setSelectedStation] = useState('FiboStation');
     const [selectedTime, setSelectedTime] = useState('none');
-    const [isFullScreen, setIsFullScreen] = useState(false);
     const navigate = useNavigate();
+    const [isFullScreen, setIsFullScreen] = useState(false);
     const [isSwitching, setIsSwitching] = useState(false);
-    const [currentContent, setCurrentContent] = useState(<FBFullscreenContent />);
 
     const toggleFullScreen = () => {
         if (!document.fullscreenElement) {
@@ -44,30 +43,26 @@ function FBLocation() {
     };
 
     useEffect(() => {
-    let intervalId;
+        let intervalId;
 
-    if (selectedTime === '10mins' && !isSwitching) {
-        toggleFullScreen();
-        setIsSwitching(true);
+        if (selectedTime === '10mins' && !isSwitching) {
+            toggleFullScreen();
+            setIsSwitching(true);
 
-        // Schedule switching between FB and LG every 10 minutes
-        const switchInterval = 10 * 60 * 1000; // 10 minutes in milliseconds
+            // Schedule switching between FB and LG every 5 seconds
+            intervalId = setInterval(() => {
+                // Toggle between FB and LG content
+                setIsSwitching((prevSwitch) => !prevSwitch);
+            }, 5000);
+        } else {
+            setIsFullScreen(false);
+            clearInterval(intervalId);
+        }
 
-        intervalId = setInterval(() => {
-            // Toggle between FB and LG content
-            setCurrentContent((prevContent) =>
-                prevContent === <FBFullscreenContent /> ? <LGFullscreenContent /> : <FBFullscreenContent />
-            );
-        }, switchInterval);
-    } else {
-        setIsFullScreen(false);
-        clearInterval(intervalId);
-    }
-
-    return () => {
-        clearInterval(intervalId);
-    };
-}, [selectedTime, isSwitching]);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [selectedTime, isSwitching]);
 
     useEffect(() => {
         if (isFullScreen) {
@@ -96,40 +91,87 @@ function FBLocation() {
                 <Select
                     value={selectedStation}
                     onChange={handleChangeStation}
-                    style={{ borderRadius: '25px', backgroundColor: "#F0F5FF", height: '45px' }}
+                    style={{
+                        borderRadius: '25px',
+                        backgroundColor: '#F0F5FF',
+                        height: '45px',
+                    }}
                     MenuProps={{
                         PaperProps: {
                             style: {
                                 borderRadius: '15px',
-                                backgroundColor: '#F0F5FF'
+                                backgroundColor: '#F0F5FF',
                             },
                         },
                     }}
                 >
-                    <MenuItem value="LGStation"> <Typography variant="h5" fontWeight="600" color="#363E64"><LGStation /></Typography></MenuItem>
-                    <MenuItem value="FiboStation"><Typography variant="h5" fontWeight="600" color="#363E64"><FiboStation /></Typography></MenuItem>
+                    <MenuItem value="LGStation">
+                        <Typography variant="h5" fontWeight="600" color="#363E64">
+                            <LGStation />
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem value="FiboStation">
+                        <Typography variant="h5" fontWeight="600" color="#363E64">
+                            <FiboStation />
+                        </Typography>
+                    </MenuItem>
                     <Grid container alignItems="center">
                         <Grid item>
-                            <MenuItem value="Switch"><Typography variant="h7" color="#363E64" fontWeight="600">Switch Location</Typography></MenuItem>
+                            <MenuItem value="Switch">
+                                <Typography
+                                    variant="h7"
+                                    color="#363E64"
+                                    fontWeight="600"
+                                >
+                                    Switch Location
+                                </Typography>
+                            </MenuItem>
                         </Grid>
                         <Grid item>
                             <FormControl>
                                 <Select
                                     value={selectedTime}
                                     onChange={handleChangeTime}
-                                    style={{ borderRadius: '25px', backgroundColor: "#FFFFF" }}
+                                    style={{
+                                        borderRadius: '25px',
+                                        backgroundColor: '#FFFFF',
+                                    }}
                                     MenuProps={{
                                         PaperProps: {
                                             style: {
                                                 borderRadius: '15px',
-                                                backgroundColor: '#FFFFF'
+                                                backgroundColor: '#FFFFF',
                                             },
                                         },
                                     }}
                                 >
-                                    <MenuItem value="none"><Typography variant="h7" color="#363E64" fontWeight="600">None</Typography></MenuItem>
-                                    <MenuItem value="10mins"><Typography variant="h7" color="#363E64" fontWeight="600">every 10 mins</Typography></MenuItem>
-                                    <MenuItem value="20mins"><Typography variant="h7" color="#363E64" fontWeight="600">every 20 mins</Typography></MenuItem>
+                                    <MenuItem value="none">
+                                        <Typography
+                                            variant="h7"
+                                            color="#363E64"
+                                            fontWeight="600"
+                                        >
+                                            None
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem value="10mins">
+                                        <Typography
+                                            variant="h7"
+                                            color="#363E64"
+                                            fontWeight="600"
+                                        >
+                                            every 10 mins
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem value="20mins">
+                                        <Typography
+                                            variant="h7"
+                                            color="#363E64"
+                                            fontWeight="600"
+                                        >
+                                            every 20 mins
+                                        </Typography>
+                                    </MenuItem>
                                 </Select>
                             </FormControl>
                         </Grid>
@@ -143,13 +185,19 @@ function FBLocation() {
                 paddingLeft="30px"
                 width="100%"
             >
-                <Typography variant="h5" fontWeight="600" color="#363E64">KMUTT, Bangkok</Typography>
+                <Typography variant="h5" fontWeight="600" color="#363E64">
+                    KMUTT, Bangkok
+                </Typography>
                 <Box mt={2}></Box>
             </Box>
 
-            {/* Conditional rendering of FBFullscreenContent or LGFullscreenContent based on isFullScreen */}
-            {isFullScreen ? (
-                selectedStation === 'FiboStation' ? <FBFullscreenContent /> : <LGFullscreenContent />
+            {/* Conditional rendering of FBFullscreenContent or LGFullscreenContent based on isSwitching */}
+            {isSwitching ? (
+                selectedStation === 'FiboStation' ? (
+                    <FBFullscreenContent />
+                ) : (
+                    <LGFullscreenContent />
+                )
             ) : null}
         </Box>
     );
